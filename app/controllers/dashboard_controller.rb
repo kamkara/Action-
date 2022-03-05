cc639b5e66e219f333bb9b1b5d604194e9b515f9
kamkara <lnclass.kamkara@gmail.com>
2022-03-05 22:47:50 +0000

    add membership dashboard query

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,8 +3,7 @@ class DashboardController < ApplicationController
   
   def index
     @MembersList = User.where("membership_category = ?", "particulier" || "organisation").order('created_at desc')
-    @MembersMonthly = @MembersList.where(:created_at => (Time.now.midnight - 30.day)..Time.now.midnight)
-    @MembersWeekly = @MembersMonthly.where(:created_at => (Time.now.midnight - 7.day)..Time.now.midnight)
+    
   end
 
   def home
--- app/controllers/membership_controller.rb
+++ app/controllers/membership_controller.rb
@@ -3,7 +3,7 @@ class MembershipController < ApplicationController
   end
   def memberslist
     @MembersList = User.where("membership_category = ?", "particulier" || "organisation").order('created_at desc')
-    @MembersMonthly = @MembersList.where("created_at >= ?", 1.month.from_now)
-    @MembersWeekly = @MembersMonthly.where("created_at >= ?", 1.week.from_now)
+    @MembersMonthly = @MembersList.where(:created_at => (Time.now.midnight - 30.day)..Time.now.midnight)
+    @MembersWeekly = @MembersMonthly.where(:created_at => (Time.now.midnight - 7.day)..Time.now.midnight)
   end
 end
